82338f09af9275b913201d8cc5e4471c3fdf8816
Martin Uribe <clamytoe@gmail.com>
2022-10-19 22:34:53 -0500

    Removes unused code

--- course-zoomcamp/cohorts/2022/07-bento-production/service.py
+++ course-zoomcamp/cohorts/2022/07-bento-production/service.py
@@ -1,17 +1,6 @@
 import bentoml
-import numpy as np
-from bentoml.io import JSON, NumpyNdarray
-from pydantic import BaseModel
+from bentoml.io import NumpyNdarray
 
-
-class UserProfile(BaseModel):
-    name: str
-    age: int
-    country: str
-    rating: float
-
-
-# tag = "mlzoomcamp_homework:qtzdz3slg6mwwdu5"
 tag = "mlzoomcamp_homework:jsi67fslz6txydu5"
 model_ref = bentoml.sklearn.get(tag)
 model_runner = model_ref.to_runner()
@@ -19,10 +8,7 @@ model_runner = model_ref.to_runner()
 svc = bentoml.Service("user_profile_classifier", runners=[model_runner])
 
 
-@svc.api(
-    input=NumpyNdarray(),
-    output=NumpyNdarray()
-)
+@svc.api(input=NumpyNdarray(), output=NumpyNdarray())
 async def classify(user_profile):
     prediction = await model_runner.predict.async_run(user_profile)
 
